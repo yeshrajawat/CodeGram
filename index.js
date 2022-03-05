6f581314ab36dccae0fcc2baf1665a9ea4cb3eab
yeshrajawat <yeshrajawat@gmail.com>
2022-03-06 02:33:19 +0530

    Added mongoose in the project

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const port = 8000;
 const app = express();
 const routes = require('./routes/index');
 const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
 
 
 
