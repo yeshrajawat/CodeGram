bdc056a59623bf0390e9000a70174a7f9d1b355c
yeshrajawat <yeshrajawat@gmail.com>
2022-03-06 20:15:26 +0530

    Created and Added User Schema

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,7 @@
+module.exports.users = function(req,res){
+    res.send("<h1>Hemlo Users</h1>")
+}
+
 module.exports.profile = function(req,res){
     res.render('user',{
         title:"Profile"
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/users_controller');
 
+
+
+router.get('/',userController.users);
+
 router.get('/profile',userController.profile);
 
 
