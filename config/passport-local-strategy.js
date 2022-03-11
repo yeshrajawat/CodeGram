0db203f93382068d54856c51272db6ec79f6e168
yeshrajawat <yeshrajawat@gmail.com>
2022-03-11 15:43:49 +0530

    added serialize and deserialize user

---
+++ config/passport-local-strategy.js
@@ -0,0 +1,44 @@
+const passport = require('passport');
+const Localstrategy  = require('passport-local').Strategy;
+const User= require('../model/user');
+
+
+//Authentication using passport
+passport.use(new Localstrategy({ 
+    usernameField:'email'
+    },
+    function(email,password,done){
+        //  Find a user and establish the identity
+        User.findOne({email:email},function(err,user){
+            if(err){
+                console.log("Error finding the user using passport");
+                return done(err,false);
+            }
+            if(!user || user.password != password){
+                
+                console.log('Invalid username or password')
+                return done(null,false);
+            }
+            else{
+                return done(null,user);
+            }
+            
+
+        })
+    }
+));
+
+passport.serializeUser( function(user,done){
+    done(null,user.id);
+})
+passport.deserializeUser(function(id,done){
+    User.findById(id, function(err,user){
+        if(err){
+            console.log("Error while finding user");
+            return done(err);
+        }
+        else{
+            return done(null,user);
+        }
+    });
+})
