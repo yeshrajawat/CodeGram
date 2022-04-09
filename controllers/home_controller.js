9f5db6b4b19644c6dce07453bce1f73a1455d9ff
yeshrajawat <yeshrajawat@gmail.com>
2022-04-09 15:14:07 +0530

    Added a two authentication factor for posting a post as an user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,17 +1,19 @@
 const Post = require('../model/post');
 module.exports.home = function(req,res){
    
-   Post.find({},function(err,allPosts){
-    if(err){
-        console.error("Error in fetching contacts from db");
+   Post.find({}).populate('user').exec(
+    function(err,allPosts){
+        if(err){
+            console.error("Error in fetching contacts from db");
+        }
+         else{
+            return res.render('home',{
+                title:"Home",
+                posts: allPosts
+            });
+         }
     }
-     else{
-        return res.render('home',{
-            title:"Home",
-            posts: allPosts
-        });
-     }
-})
+   )
 };
 
 
--- routes/post_route.js
+++ routes/post_route.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-
+const passport = require('passport');
+const Localpassport = require('../config/passport-local-strategy')
 const postController = require('../controllers/posts_controller');
 
-router.post('/create',postController.create);
+router.post('/create',Localpassport.checkAuthentication,postController.create);
 module.exports = router; 
